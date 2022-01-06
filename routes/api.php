b719a5c9ed2f389874353dfa4ff2f805e161d0f7
jordan.lopez <imjordanlopez@gmail.com>
2022-01-06 16:09:04 +0800

    login/register

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\LoginController;
+use App\Http\Auth\Controllers\RegisterController;
+use App\Http\Auth\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
