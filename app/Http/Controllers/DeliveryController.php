c8cb0a860608083b38b56709cde9468d3335cbba
jordan.lopez <imjordanlopez@gmail.com>
2022-01-19 14:35:48 +0800

    transactions table - search and pagination

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -25,14 +25,20 @@ class DeliveryController extends Controller
     public function search()
     {
         $key = \Request::get('q');
-        $delivery = Delivery::where('delivery_id','LIKE',"%{$key}%")
+        $entries = \Request::get('entries');
+        $page_number = $entries;
+
+        $deliveries = Delivery::where('delivery_id','LIKE',"%{$key}%")
         ->orWhere('delivery_date','LIKE',"%{$key}%")
         ->orWhere('origin','LIKE',"%{$key}%")
         ->orWhere('destination','LIKE',"%{$key}%")
         ->orWhere('cost','LIKE',"%{$key}%")
         ->orWhereRaw("(CASE WHEN status = 1 THEN 'Delivered' WHEN status = 2 THEN 'In Transit' ELSE 'To Deliver' END) LIKE '%{$key}%'")
-        ->get();
+        ->paginate($page_number);
 
-        return response()->json(['delivery' => $delivery], 200);
+        return response()->json(
+            new DeliveriesCollection($deliveries),
+            Response::HTTP_OK
+        );
     }
 }
