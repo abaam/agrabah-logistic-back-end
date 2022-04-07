30e0b73b4e05e30496863d66b8f0654271d7ce1a
Xavier Emmanuel Rebotica <seybyer.emanwel@gmail.com>
2022-04-07 11:04:30 +0800

    Add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             DeliverySeeder::class,
+            UserSeeder::class
         ]);
     }
 }
