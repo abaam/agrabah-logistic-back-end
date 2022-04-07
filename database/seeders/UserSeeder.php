94c0aea9efa702be1b9e59de50d21da43f0873e2
Xavier Emmanuel Rebotica <seybyer.emanwel@gmail.com>
2022-04-08 00:22:21 +0800

    Add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+        	'phone_number' => '09669510498',
+            'password' => bcrypt('driver@123'),
+            'register_as' => 1,
+            'pin' => '000000',
+            'verified' => 1
+        ]);
+
+        User::create([
+        	'phone_number' => '09108108998',
+            'password' => bcrypt('customer@123'),
+            'register_as' => 2,
+            'pin' => '101010',
+            'verified' => 1
+        ]);
+    }
+}
