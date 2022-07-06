311b322aa7730ace5540d94a23dea710590060e7
jordan-lopez <imjordanlopez@gmail.com>
2022-07-06 17:00:42 +0800

    create booking

--- config/sanctum.php
+++ config/sanctum.php
@@ -13,13 +13,19 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', implode(',', [
-        'localhost',
-        'localhost:8000',
-        '127.0.0.1',
-        '127.0.0.1:8000',
-        'api.agrabah.ph',
-    ]))),
+    // 'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', implode(',', [
+    //     'localhost',
+    //     'localhost:8000',
+    //     '127.0.0.1',
+    //     '127.0.0.1:8000',
+    //     'api.agrabah.ph',
+    // ]))),
+
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+        '%s%s',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,api.agrabah.ph',
+        env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
+    ))),
 
     /*
     |--------------------------------------------------------------------------
@@ -33,7 +39,7 @@ return [
     |
     */
 
-    // 'guard' => ['api'],
+    'guard' => ['web'],
 
     /*
     |--------------------------------------------------------------------------
